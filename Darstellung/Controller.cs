f69eabccdd897249bddd1d47275c4afec925c55d
Paul Olbrich <Paul.Olbrich@sennheiser.com>
2022-01-10 16:46:20 +0100

    Darstellung funzt nicht komplett

--- Darstellung/Controller.cs
+++ Darstellung/Controller.cs
@@ -9,14 +9,47 @@ namespace Darstellung
 {
     public static class Controller
     {
-        static List<Punkt> punkte;
+        public static List<Punkt> punkte = new List<Punkt>();
 
         public static void AddPunkt(double x, double yAlt)
         {
             double y;
             y = KoordinatenRechner.KoordinatenUmrechnen(yAlt);
 
-            Punkt p = new Punkt(x,y);
+            Punkt p = new Punkt(x, y);
+
+
+            if (punkte.Count > 1) // 2 oder mehr
+            {
+                for (int i = 0; i < punkte.Count; i++)
+                {
+                    if (p.x < punkte[i].x)
+                    {
+                        punkte.Insert(i, p);
+                        break;
+                    }
+                }
+            }
+            else if (punkte.Count > 0)
+            {
+                if (punkte[0].x < p.x)
+                    punkte.Add(p);
+                else
+                    punkte.Insert(0, p);
+            }
+            else
+                punkte.Add(p);
+        }
+
+        public static List<Punkt> GetZuZeichendePunkte()
+        {
+            List<Punkt> punkteWelt = PunkteRechner.BerechneZuZeichnendePunkte(punkte, KoeffizientenRechner.BerechneKoeffizenten(punkte));
+            foreach (Punkt p in punkteWelt)
+            {
+                p.y = KoordinatenRechner.KoordinatenUmrechnen(p.y);
+            }
+
+            return punkteWelt;
         }
     }
 }
--- Darstellung/MainWindow.xaml.cs
+++ Darstellung/MainWindow.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Splines;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -23,16 +24,57 @@ namespace Darstellung
         public MainWindow()
         {
             InitializeComponent();
-            
+
         }
 
         private void Canvas_MouseDown(object sender, MouseButtonEventArgs e)
         {
+            StelleBelegt stelleBelegt = new StelleBelegt();
+
             //Check if Click auf selber stelle wie anderer Punkt
+            if (Controller.punkte is not null)
+                for (int i = 0; i < Controller.punkte.Count; i++)
+                    if (Mouse.GetPosition(MyCanvas).X == Controller.punkte[i].x)
+                    {
+                        stelleBelegt.Show();
+                        return;
+                    }
 
+            //MyCanvas.Children.Clear();
             //Füge punkt in Liste hinzu
+            Controller.AddPunkt(Mouse.GetPosition(MyCanvas).X, Mouse.GetPosition(MyCanvas).Y);
 
-            //stelle Punkte da / mit Button Click Splines aktualisieren
+            Ellipse ellipse = new Ellipse()
+            {
+                Fill = Brushes.Black,
+                Height = 4,
+                Width = 4
+            };
+
+            //Punkte Darstellung
+            Canvas.SetLeft(ellipse, Mouse.GetPosition(MyCanvas).X - 2);
+            Canvas.SetTop(ellipse, Mouse.GetPosition(MyCanvas).Y - 2);
+            MyCanvas.Children.Add(ellipse);
+
+            //get zu ZeichnendePunkte  -> Darstellen
+            if (Controller.punkte.Count > 3)
+            {
+                List<Punkt> zuZeichnen = Controller.GetZuZeichendePunkte();
+                for (int i = 0; i < zuZeichnen.Count - 1; i++)
+                {
+                    Line line = new Line()
+                    {
+                        Stroke = Brushes.Black,
+                        StrokeThickness = 2,
+                        X1 = zuZeichnen[i].x,
+                        X2 = zuZeichnen[i + 1].x,
+                        Y1 = zuZeichnen[i].y,
+                        Y2 = zuZeichnen[i + 1].y
+                    };
+
+                    MyCanvas.Children.Add(line);
+                }
+            }
         }
     }
 }
